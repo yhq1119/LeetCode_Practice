db91b544ad2afa6cfa280bb393704b5c82539506
rmit-s3681278-haoqian-yang <42133486+rmit-s3681278-haoqian-yang@users.noreply.github.com>
2020-02-13 03:24:04 +1100

    update

---
+++ src/Others/CounterBinarySubstrings.java
@@ -0,0 +1,22 @@
+package Others;
+
+public class CounterBinarySubstrings {
+
+    int sum=0;
+    int change;
+    char start = ' ';
+    public static void main(String[] args) {
+
+    }
+
+    public int countBinarySubstrings(String s){
+    // detect the adjacent chars
+
+
+
+        return sum;
+    }
+
+
+
+}
---
+++ src/Test/MethodTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+public class MethodTest {
+    @Test
+    public void test1(){
+        String test1 = "1110000";
+        String test2 = "1111000";
+        String test3 = "10";
+
+
+    }
+
+    private void detectFromIndex(int i){
+
+    }
+
+}
