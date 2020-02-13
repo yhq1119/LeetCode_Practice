5cf0cca2d7ca92240a6e955949e3e97fba08389d
rmit-s3681278-haoqian-yang <42133486+rmit-s3681278-haoqian-yang@users.noreply.github.com>
2020-02-14 06:15:58 +1100

    good

--- src/Others/GuessTheNthChar.java
+++ src/Others/GuessTheNthChar.java
@@ -4,14 +4,115 @@ public class GuessTheNthChar {
 
     public static void main(String[] args) {
 
+        String str1 = "a[b]2cd[ef]5ghi";
+        String str2 = "a[b[c[ok]2]2]2"; // ab[c]2b[c]2b[c]2 --> abccbccbcc
+
+        new GuessTheNthChar().showAt(str1,4);
+        new GuessTheNthChar().showAt(str2,5);
+
+
+//        System.out.println("fin " + test1);
+//        System.out.println("fin " + test2);
+
+
     }
 
-    public char guess(String str, int n){
-        char temp=' ';
+    private void showAt(String str, int n){
+
+        String detect = detect(str);
+
+        if (str!=null&&detect.length()>0){
+            if (n>0&&n<detect.length()+1){
+                System.out.println(
+                        "The character of "+str+" at ["+n+"] is ["+detect.charAt(n-1)+"]"
+                );}
+            else {
+                System.out.println("Not a valid n");
+            }
+        }else {
+            System.out.println("Not a valid String");
+        }
+    }
+
+    private String detect(String str) {
+
+        String copy = str;
+        int count = 0;
+
+        boolean startBrace = false;
+        int startIndex = 0;
+
+        for (int i = 0; i < copy.length(); i++) {
+
+            if (!startBrace) {
+
+                if (copy.charAt(i) == '[') {
+                    startBrace = true;
+                    startIndex = i;
+                }
+            } else {
+                if (copy.charAt(i) == ']') {
+                    String temp = copy.substring(startIndex, i)
+                            .replaceAll("\\[", "")
+                            .replaceAll("]", "");
+
+                    int k = i;
+                    k = k + 1;
+                    String digit = "";
 
 
+                    if (k > copy.length() - 1) {
 
-        return temp;
+                    } else {
+//                        System.out.println(k);
+                        while (Character.isDigit(copy.charAt(k))) {
+                            digit = digit + (copy.charAt(k));
+
+                            k++;
+                            if (k > copy.length() - 1) {
+                                break;
+                            }
+                        }
+                    }
+
+                    int times = Integer.parseInt(digit);
+                    String t = "";
+                    while (times > 0) {
+                        t = t + temp;
+                        times--;
+                    }
+                    startBrace = false;
+                    copy = copy.substring(0, startIndex) + t + copy.substring(k);
+
+                } else if (copy.charAt(i) == '[') {
+                    startIndex = i;
+                    count++;
+                    startBrace = true;
+                }
+
+            }
+
+        }
+
+//        printArray(words);
+
+//        System.out.println(copy);
+        while (count > 0) {
+            copy = detect(copy);
+            count--;
+        }
+
+        return copy;
 
     }
+
+//    private void printArray(ArrayList list) {
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i) != null) {
+//
+//                System.out.print(list.get(i) + " , ");
+//            }
+//        }
+//        System.out.println();
+//    }
 }
